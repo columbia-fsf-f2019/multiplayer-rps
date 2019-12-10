1043d5a8b812153ed71b9364c796f12eeb7e61e0
Michael Gelber <mpgelber7495@gmail.com>
2019-12-09 19:01:42 -0500

    Full database game start complete

--- assets/apps.js
+++ assets/apps.js
@@ -15,6 +15,13 @@ function writeData(key, value) {
     .set(data, { merge: true });
 }
 
+function resetGame() {
+  writeData("playerOne", "null");
+  writeData("playerTwo", "null");
+}
+
+resetGame();
+
 // Upon page load check for whether or not other players are registered
 db.collection("players")
   .doc("x403VpJmjGFGDJvo88UY")
@@ -27,11 +34,17 @@ db.collection("players")
       qs("#add-user").onclick = function(event) {
         event.preventDefault();
         writeData("playerOne", qs("#name-input").value);
+        qs("#waiting-player").innerText = `You're all set! Hang tight`;
+        qs("#player-form").style.display = "none";
       };
     } else if (playerTwo === "null") {
       // Only playerOne has joined the game
       qs("#player-form").style.display = "block";
       qs("#waiting-player").innerText = `${playerOne} is waiting for you!`;
+      qs("#add-user").onclick = function(event) {
+        event.preventDefault();
+        writeData("playerTwo", qs("#name-input").value);
+      };
     } else {
       // The game is in session
       qs("#player-form").style.display = "none";
